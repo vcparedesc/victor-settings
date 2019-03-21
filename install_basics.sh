1480b63f4055704e0f698cac3df995ec88473019
Victor Paredes <pvictorm6@gmail.com>
2019-03-20 22:00:30 -0500

    update

--- install_basics.sh
+++ install_basics.sh
@@ -25,5 +25,6 @@ sudo apt install zsh -y
 sudo apt install fonts-powerline -y
 sudo apt install g++-6 -y
 sudo apt install tmux -y
+sudo apt install wine1.6 -y
 
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
