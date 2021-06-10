d2612b4eb20607949e306498852cda5a0cc39ed9
Victor Paredes <pvictorm6@gmail.com>
2021-06-10 03:13:15 -0400

    adding eigendb and rtags on install_basics

--- install_basics.sh
+++ install_basics.sh
@@ -10,6 +10,9 @@ cd -
 sudo apt update
 sudo apt upgrade
 
+mkdir Repositories
+mkdir -p Research/Repositories
+
 sudo apt install git -y
 sudo apt install meld -y
 sudo apt install konsole -y
@@ -26,7 +29,7 @@ sudo apt install fonts-powerline -y
 sudo apt install g++-6 -y
 sudo apt install tmux -y
 sudo apt install wine1.6 -y
-sudo apt install texlive-full -y
+#sudo apt install texlive-full -y
 
 # To provide functionality for elpy:
 pip install jedi
@@ -39,12 +42,19 @@ sudo add-apt-repository ppa:apandada1/xournalpp-stable
 sudo apt update
 sudo apt install xournalpp
 
-
+cd Repositories
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 git clone https://github.com/jorgenschaefer/pyvenv.git
+
+git clone git@github.com:dmillard/eigengdb.git
+cd eigendb
+sudo python setup.py install # Make sure to use system python which matches GDB
+python bin/eigengdb_register_printers
+
+cd ..
+git clone --recursive https://github.com/Andersbakken/rtags.git
+cd rtags
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 .
+make
+
 cd ~/
-
-mkdir -p Repositories
-cd Repositories
-
-
